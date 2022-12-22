a5adbaad026a98ee577179318f63d71768cef25b
Shoya Mitomi <s21C1119XH@s.chibakoudai.jp>
2022-12-23 01:13:54 +0900

    add github test

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='root',
-    maintainer_email='shoya4108@icloud.com',
+    maintainer_email='s21C1119XH@s.chibakoudai.jp',
     description='a package for practice',
     license='BSD-3-Clause',
     tests_require=['pytest'],
