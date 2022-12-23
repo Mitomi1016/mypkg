1e3dcba91acb4a4f6a8e107382ec3193e78eae9e
Shoya Mitomi <s21C1119XH@s.chibakoudai.jp>
2022-12-23 15:38:07 +0900

    update launch

--- launch/talk_listen.launch.py
+++ launch/talk_listen.launch.py
@@ -1,8 +1,10 @@
+# SPDX -FileCopyrightText:2022 Shoya Mitomi
+# SPDX-License-Identifier: BSD-3-Clause
+
 import launch
 import launch.actions
 import launch.substitutions
 import launch_ros.actions
-
 
 def generate_launch_description():
 
